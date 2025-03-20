d712148338e21b961d0653ff90d0d19e738b8f5b
ktg3319 <30924220+ktg3319@users.noreply.github.com>
2025-03-20 12:39:53 +0900

    Add files via upload

---
+++ source/get_lib.R
@@ -0,0 +1,12 @@
+library(fpp3)
+library(tidyverse)
+library(patchwork)
+library(kableExtra)
+library(tsibble)
+library(wooldridge)
+library(plotly)
+library(lubridate) #yearweek()
+
+#reading data
+library(data.table)
+library(fable.prophet)
